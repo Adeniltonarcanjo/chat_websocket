0bc7e5c1d1db981850ddc1980ca54b7f572de5a1
Adeniltonarcanjo <adeniltonmorais@gmail.com>
2021-09-24 22:50:47 -0300

    Primeira versão completa

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+//tornando o diretório public usavél pelo express
 app.use(express.static('public'));
 
 const http = require('http').Server(app);
@@ -9,29 +10,42 @@ const http = require('http').Server(app);
 const serverSocket = require('socket.io')(http);
 
 
-const porta=8000
+const PORT=3000|| process.env.PORT;
 
-http.listen(porta, function(){
-console.log("servidor iniciado na porta 8000");
+http.listen(PORT, function(){
+
+console.log("servidor iniciado na porta 3000");
 
 });
 
+// ao acessar a raiz do site , carregar a pagina html
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 })
 
-serverSocket.on('connection', function(socket){
+
+
+serverSocket.on('connection', function(socket){    
+
     
-
-    socket.on('chat msg', function(msg){
-        console.log(`Msg recebida do cliente ${socket.io}: ${msg}`);
-        serverSocket.emit('chat msg',`Usuário ${nickname} conectou.`)
-    })
-
     socket.on('login', function(nickname){
-        console.log('cliente conectado '+nickname)
-        serverSocket.emit('chat msg', msg)
+        //console.log('cliente conectado '+nickname)
+        serverSocket.emit('chat msg',  `${nickname} entrou na sala`)
+        socket.nickname=nickname
+    })
+   
+    
+    // ao executar mensagem chat msg, executar a função
+    socket.on('chat msg', function(msg){
+      //  console.log(`Msg recebida do cliente ${socket.nickname}: ${msg}`);
+        serverSocket.emit('chat msg',`${socket.nickname}: ${msg}`);
     })
 
-})
+    socket.on('status', function(msg){
+        //  console.log(`Msg recebida do cliente ${socket.nickname}: ${msg}`);
+          socket.broadcast.emit('status',msg);
+      })
+
+    
+});
 
